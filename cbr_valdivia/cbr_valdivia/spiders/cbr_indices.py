bd5dff3d5dae81e324d6cc350cad2c593df3bc63
fitogabo <gapantoj@uc.cl>
2025-02-17 01:25:30 -0300

    Implementar extracción de datos mediante Selenium en CbrIndicesSpider y actualizar la lógica de solicitudes POST.

--- cbr_valdivia/cbr_valdivia/spiders/cbr_indices.py
+++ cbr_valdivia/cbr_valdivia/spiders/cbr_indices.py
@@ -1,26 +1,48 @@
 import scrapy
 from scrapy.http import FormRequest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from scrapy.selector import Selector
 
 class CbrIndicesSpider(scrapy.Spider):
     name = 'cbr_indices'
     allowed_domains = ['www.cbrvaldivia.cl']
     start_urls = ['http://www.cbrvaldivia.cl/indices']
 
-    def start_requests(self):
-        # Enviar solicitud POST con los parámetros de año
-        return [FormRequest(url=self.start_urls[0],
-                            formdata={'anio_desde': '2020', 'anio_hasta': '2025'},
-                            callback=self.parse)]
+    def __init__(self, *args, **kwargs):
+        super(CbrIndicesSpider, self).__init__(*args, **kwargs)
+        self.driver = webdriver.Chrome()  # Asegúrate de tener el controlador de Chrome instalado
 
-    def parse(self, response):
-        # Imprimir el HTML para depuración
-        self.logger.debug(response.text)
+    def start_requests(self):
+        self.driver.get(self.start_urls[0])
+        
+        # Enviar solicitud POST con los parámetros de año
+        anio_desde = self.driver.find_element(By.ID, 'anio_registro')
+        anio_hasta = self.driver.find_element(By.ID, 'anio_registro2')
+        anio_desde.send_keys('2020')
+        anio_hasta.send_keys('2025')
+        
+        consultar_button = self.driver.find_element(By.ID, 'consultar_ind')
+        consultar_button.click()
+        
+        # Esperar a que la ventana modal se cargue
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'modal-miventana'))
+        )
         
         # Extraer datos de la ventana modal
-        modal = response.css('#modal-miventana')
-        indices = modal.css('table.indices')
+        self.parse(self.driver.page_source)
         
-        for indice in indices.css('tr'):
+        self.driver.quit()
+
+    def parse(self, html):
+        response = Selector(text=html)
+        modal = response.css('#modal-miventana')
+        indices = modal.css('div.content > div.table-responsive-sm > table > tbody > tr')
+        
+        for indice in indices:
             yield {
                 'foja': indice.css('td:nth-child(1)::text').get(),
                 'numero': indice.css('td:nth-child(2)::text').get(),
